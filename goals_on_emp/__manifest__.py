16268357ef32baefa22ff90864609a41cbd4f306
Manu S <124423662+manu-git-hub@users.noreply.github.com>
2023-05-22 10:41:32 +0530

    Fixed security Bugs

--- goals_on_emp/__manifest__.py
+++ goals_on_emp/__manifest__.py
@@ -1,37 +1,33 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "Goals",
+    'name': "goal_leuwint",
 
     'summary': """
-        Adds new fields to the Employee model""",
+        Employees and Company Goals """,
 
     'description': """
-    TAdded new field Goal   """,
+        Goals for Employees
+    """,
 
-    'author': "Leuwint Technology",
-    'website': "https://www.leuwint.com/",
+    'author': "Leuwint Technologies",
+    'website': "https://leuwint.com/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Human Resources',
-    'version': '1.1',
-
+    'category': 'Goal',
+    'version': '1.0.0',
     # any module necessary for this one to work correctly
     'depends': ['base', 'hr'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/employee_goal_views.xml',
-        # 'views/hr_employee_views.xml',
-        
+        'security/ir.model.access.csv',
+        'security/security.xml',
+        'views/menu.xml',
     ],
+    'license': 'LGPL-3',
+    'application':True,
+    'auto_install': False,
     'installable': True,
-    'auto_install'  : False,
-   # only loaded in demonstration mode
-    # 'demo': [
-    #     'demo/demo.xml', 
-    # ],
-    
 }
--- goals_on_emp/models/__init__.py
+++ goals_on_emp/models/__init__.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import employee_goal
+from . import goal
